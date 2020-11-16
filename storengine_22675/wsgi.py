7b423c7c6860abed554a9ca9eb4de8dce9f3fd07
Crowdbotics <team@crowdbotics.com>
2020-11-16 16:51:29 +0000

    Initial commit.

---
+++ storengine_22675/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for storengine_22675 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'storengine_22675.settings')
+
+application = get_wsgi_application()
